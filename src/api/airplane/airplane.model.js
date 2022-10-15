1971611808c117c84559568aeeb0c950b18eaacd
Noah <zap.noah@gmail.com>
2022-10-14 23:55:51 -0500

    update airplane mode

--- src/api/airplane/airplane.model.js
+++ src/api/airplane/airplane.model.js
@@ -1,10 +1,23 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, models } = require('mongoose');
 
 const airplaneSchema = new Schema(
   {
     plate: {
       type: String,
       required: true,
+      validate: [
+        {
+          async validator(value) {
+            try {
+              const plane = await models.Airplane.findOne({ plate: value });
+              return !plane;
+            } catch {
+              return false;
+            }
+          },
+          message: 'It is already exist a airplane with this plate code',
+        },
+      ],
     },
     planeModel: {
       type: String,
